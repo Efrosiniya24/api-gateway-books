99d0e6f0e03cac5f45e8aa0bc4bf1ea679661f78
Efrosiniya24 <efrosiniya24@gmail.com>
2025-01-25 19:05:00 +0300

    start api gateway

---
+++ src/main/java/org/project/apigatewaybooks/ApiGatewayBooksApplication.java
@@ -0,0 +1,13 @@
+package org.project.apigatewaybooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayBooksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/project/apigatewaybooks/ApiGatewayBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.apigatewaybooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayBooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
