1329c562115a601f18fff8a1ff4d45a2f6196b11
Efrosiniya24 <efrosiniya24@gmail.com>
2025-01-26 14:25:47 +0300

    type

--- src/main/java/org/project/apigatewaybooks/controller/GatewayController.java
+++ src/main/java/org/project/apigatewaybooks/controller/GatewayController.java
@@ -16,14 +16,14 @@ public class GatewayController {
     private final RestTemplate restTemplate;
 
     @GetMapping("/books/book-storage/{endpoint}")
-    public ResponseEntity<?> proxyToBookStorage(@PathVariable String endpoint, @RequestHeader HttpHeaders headers) {
+    public ResponseEntity<String> proxyToBookStorage(@PathVariable String endpoint, @RequestHeader HttpHeaders headers) {
         String url = "http://localhost:8082/books/book-storage/" + endpoint;
         HttpEntity<String> requestEntity = new HttpEntity<>(headers);
         return restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class);
     }
 
     @GetMapping("/books/book-tracker/{endpoint}")
-    public ResponseEntity<?> proxyToBookTracker(@PathVariable String endpoint, @RequestHeader HttpHeaders headers) {
+    public ResponseEntity<String> proxyToBookTracker(@PathVariable String endpoint, @RequestHeader HttpHeaders headers) {
         String url = "http://localhost:8081/books/book-tracker/" + endpoint;
         HttpEntity<String> requestEntity = new HttpEntity<>(headers);
         return restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class);
