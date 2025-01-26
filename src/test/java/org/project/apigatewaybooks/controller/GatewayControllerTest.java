92bb325bf7a5f16c528ec06d9b7217d0bb28ff40
Efrosiniya24 <efrosiniya24@gmail.com>
2025-01-26 14:30:44 +0300

    proxyToBookStorage

---
+++ src/test/java/org/project/apigatewaybooks/controller/GatewayControllerTest.java
@@ -0,0 +1,46 @@
+package org.project.apigatewaybooks.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
+class GatewayControllerTest {
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @InjectMocks
+    private GatewayController gatewayController;
+
+
+    @Test
+    void whenProxyToBookStorage_thenCallRestTemplate() {
+        //given
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer valid_token");
+        String endpoint = "/all-books";
+        String url = "http://localhost:8082/books/book-storage/" + endpoint;
+
+        //when
+        when(restTemplate.exchange(url, HttpMethod.GET, new HttpEntity<>(headers), String.class))
+                .thenReturn(ResponseEntity.ok("Success"));
+        //then
+        ResponseEntity<String> response = gatewayController.proxyToBookStorage(endpoint, headers);
+
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals("Success", response.getBody());
+
+        verify(restTemplate).exchange(url, HttpMethod.GET, new HttpEntity<>(headers), String.class);
+    }
+
+}
